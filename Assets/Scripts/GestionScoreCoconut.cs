ced8798ff58a5b045efea414b402fc82c573c3b2
JeremyEmondLapierre <justkoly@gmail.com>
2021-10-24 15:19:23 -0400

    VR
    Ajout de la gestion VR! Yayyy ( début de l'ajout du score!)

---
+++ Assets/Scripts/GestionScoreCoconut.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionScoreCoconut : MonoBehaviour
+{
+    public GameObject score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision infoCollision){
+        if(infoCollision.gameObject.name== "Bananafeu(Clone)" || infoCollision.gameObject.name== "Bananaglace(Clone)" || infoCollision.gameObject.name== "BananeDoree(Clone)" || infoCollision.gameObject.name== "Regulobanana(Clone)"){
+            score.GetComponent<TextMesh>().text += 1;
+        }
+    }
+}
